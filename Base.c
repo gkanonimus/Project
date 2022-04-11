4b2501fa7afc17d494b552f0598273f53d1250ca
gkanonimus <kiryanov.ilya@bk.ru>
2022-04-11 22:25:23 +0300

    Project base

---
+++ Base.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int n;                                  //array size without duplicate
+void print(int array[n + 1], int n);
+
+
+int main()
+{
+    printf("Enter n size: ");
+    scanf("%d", &n);
+    
+    int array[n + 1];
+    for (int i = 0; i < n; i++)          //array filling
+    {
+        scanf("%d", &array[i]);
+    }
+    
+    int dupl;
+    printf("Enter duplicate element: "); //1 <= dupl <= n
+    scanf("%d", &dupl);
+    array[n] = dupl;
+    
+    print(array, n);
+}
+
+void print(int array[n + 1], int n)
+{
+    printf("\n");
+    for (int i = 0; i < n + 1; i++)
+    {
+        printf("%d ", array[i]);
+        if (i % 10 == 0)
+        {
+            printf("\n");
+        }
+    }
+    printf("\n");
+    return;
+}
